80979bcb7ead5f7856903f6452b84952e65ff996
AlexST <at13g24@soton.ac.uk>
2026-09-07 12:56:37 +0100

    Update car.py

--- car.py
+++ car.py
@@ -5,17 +5,17 @@ class Car:
                  chassis_tyre_modifier=1.0):
         self.chassis_name = chassis_name
 
-        # 1. Fuel
+        # Fuel
         self.starting_fuel_kg = starting_fuel_kg
         self.current_fuel_kg = starting_fuel_kg
         self.base_fuel_burn_rate = base_fuel_burn_rate
         self.fuel_effect_coeff = fuel_effect_coeff
 
-        # 2. Power Unit temp
+        # Power Unit temp
         self.engine_temp = 100.0  # Clean air temp
         self.engine_redline = 125.0
 
-        # 3. Chassis Modifier
+        # Chassis Modifier
         self.chassis_tyre_modifier = chassis_tyre_modifier
 
     def process_lap(self, track_object, gap_to_car_ahead=5.0,
@@ -25,7 +25,7 @@ class Car:
         and exponential thermal plateaus.
         """
 
-        # A. Lift and Coast
+        # Lift and Coast
         actual_fuel_burn = self.base_fuel_burn_rate * (1.0 -
                                                        (lift_and_coast_pct
                                                         / 100.0))
@@ -34,11 +34,11 @@ class Car:
         # L&C costs lap time (10% lift costs ~0.3s, - needs changing to be based on track)
         lnc_time_penalty = (lift_and_coast_pct / 10.0) * 0.3
 
-        # B. Fuel loss, laptime gain
+        # Fuel loss, laptime gain
         kg_lost = self.starting_fuel_kg - self.current_fuel_kg
         weight_time_advantage = -(kg_lost * self.fuel_effect_coeff)
 
-        # C. The baseline temp car "wants" to be at in clean air
+        # The baseline temp car "wants" to be at in clean air
         target_engine_temp = 100.0
 
         # Dirty air raises the target temp
@@ -58,7 +58,7 @@ class Car:
         # Hard floor so engine doesn't freeze
         self.engine_temp = max(90.0, self.engine_temp)
 
-        # D. Instead of a blunt cliff, the hybrid system progressively trims power
+        # Instead of a blunt cliff, the hybrid system progressively trims power
         # above 115C to protect the ICE, costing small fractions of a second.
         derating_penalty = 0.0
         if self.engine_temp > 115.0:
@@ -67,7 +67,7 @@ class Car:
             # e.g., at 120C, penalty is 0.25s per lap.
             derating_penalty = over_temp * 0.05
 
-# E. Net lap time calculation
+# Net lap time calculation
         net_car_time_impact = (
             weight_time_advantage
             + lnc_time_penalty
