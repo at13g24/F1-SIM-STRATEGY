3c14495613dd9827c3303634a13f976809ca9a69
AlexST <at13g24@soton.ac.uk>
2026-09-24 14:48:37 +0100

    Update outlap_data.py

--- outlap_data.py
+++ outlap_data.py
@@ -3,26 +3,26 @@ import fastf1
 import fastf1.plotting
 import matplotlib.pyplot as plt
 
-# 1. Setup and Cache
+# Setup and Cache
 fastf1.Cache.enable_cache('f1_cache')
 fastf1.plotting.setup_mpl()  # Turns on F1-style dark mode graphs
 
 # Example using 2025 Barcelong Race. Looking at outlap data. To be changed by track.
 
-# 2. Load the 2025 Barcelona Race
+# Load 2025 Barcelona Race
 print("Loading 2025 Barcelona Race Data...")
 session = fastf1.get_session(2025, 'Spain', 'R')
 session.load(telemetry=False, weather=False)
 
-# 3. Isolate Out-Laps
+# Isolate Out-Laps
 laps = session.laps
 # An out-lap is defined as any lap that has a recorded "Pit Out Time"
 out_laps = laps[laps['PitOutTime'].notna()]
 
-# Convert the lap times from Python 'timedelta' objects into raw seconds
+# Convert the lap times from Python 'timedelta' objects into seconds
 out_lap_times = out_laps['LapTime'].dt.total_seconds().dropna()
 
-# 4. Clean the Data (Remove Safety Cars)
+# Clean the Data (Remove Safety Cars)
 green_flag_out_laps = out_lap_times[out_lap_times < 105]
 
 print(
@@ -30,7 +30,7 @@ print(
       f"green-flag out-laps."
       )
 
-# 5. Plot Histogram
+# Plot Histogram
 plt.figure(figsize=(10, 6))
 plt.hist(green_flag_out_laps, bins=15, color='cyan', edgecolor='white',
          alpha=0.8)
